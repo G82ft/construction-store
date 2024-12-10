779170cf0c21555273c4251d0c857d301edadbaa
G4m3-80ft <leonidkhvan123@gmail.com>
2024-12-11 00:57:32 +0300

    Add viewing and data

---
+++ sln/ConstructionStore/ConstructionStore/Pages/View.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ConstructionStore.Pages
+{
+    public partial class View : Page
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+    }
+}
